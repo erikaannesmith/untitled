050e652ffe54c464565263dc8cc17acc81cbba05
Iza Jimenez <izavetha@gmail.com>
2017-10-22 17:43:47 -0600

    Built item_spec validations - invalid/valid

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   validates :max_due_date, :designer_id, :specs, presence: true
   belongs_to :designer
   belongs_to :category
+  has_many :items
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Item do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without name" do
+        designer = Designer.new(email:        "company@gmail.com",
+                                password:     "password",
+                                company_name: "Company1",
+                                website:      "www.company1.com",
+                                description:  "Company1shtuff like makindaclothes",
+                                location:     "San Fransisco")
+        order = designer.orders.new(max_due_date: "10-10-2016",
+                                    specs: "Hi")
+        item = order.items.new(quantity: 2)
+
+        expect(item).to be_invalid
+      end
+
+      it "is invalid without quantity" do
+        designer = Designer.new(email:        "company@gmail.com",
+                                password:     "password",
+                                company_name: "Company1",
+                                website:      "www.company1.com",
+                                description:  "Company1shtuff like makindaclothes",
+                                location:     "San Fransisco")
+        order = designer.orders.new(max_due_date: "10-10-2016",
+                                    specs: "Hi")
+        item = order.items.new(name: "Skirt")
+
+        expect(item).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with name and quantity" do
+        designer = Designer.new(email:        "company@gmail.com",
+                                password:     "password",
+                                company_name: "Company1",
+                                website:      "www.company1.com",
+                                description:  "Company1shtuff like makindaclothes",
+                                location:     "San Fransisco")
+        order = designer.orders.new(max_due_date: "10-10-2016",
+                                    specs: "Hi")
+        item = order.items.new(name: "Skirt",
+                           quantity: 2)
+
+        expect(item).to be_valid
+      end
+    end
+  end
+
+  
 end
