e1e9d4fe470e0ba8e67494f8fc4dea97643355f0
Erika Smith <erikaannesmith@gmail.com>
2017-10-22 19:41:52 -0600

    Update routes for accessibility/restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ Rails.application.routes.draw do
 
   resources :designers do
     resources :orders do
-      resources :applications
-      resources :items
+      resources :applications, only: [:index, :show]
+      resources :items, only: [:index, :create, :new, :destroy]
     end
   end
 
-  resources :categories do
+  resources :categories, except: [:destroy] do
     resources :orders do
-      resources :items
+      resources :items, only: [:index, :create, :new, :destroy]
     end
   end
 
   resources :producers do
-    resources :applications
+    resources :applications, except: [:edit, :update]
   end
 
 end
