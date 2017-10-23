93b9b2de991975b92b5a4c91e181e461cade2a99
Iza Jimenez <izavetha@gmail.com>
2017-10-23 12:36:29 -0600

    Added 'through' relations in invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,5 @@
 class Invoice < ApplicationRecord
     validates :payment_due_date, :order_status, :payment_status, :price, :due_date, presence: true
-    belongs_to :application
-    belongs_to :order
+    belongs_to :producer, through: :application
+    belongs_to :designer, through: :order
 end
